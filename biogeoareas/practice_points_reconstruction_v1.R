426551e198d3744367bbf6fcb10c297db214758b
masafumi358 <bergentruckung358@gmail.com>
2025-04-09 13:00:16 +1200

    added biogeoareas

---
+++ biogeoareas/practice_points_reconstruction_v1.R
@@ -0,0 +1,50 @@
+
+
+library(openxlsx)
+library(rgplates)
+
+wd = "/Users/moba692/Documenting_R/GPlates_R_for_Biologists"
+setwd(wd)
+
+
+fn = "example_area_points_v1.xlsx"
+xls = openxlsx::read.xlsx(fn, sheet="Dupin2017fig5")
+xls
+
+ages = seq(from=1, to=50, by=1)
+reconstructed_points_table = xls
+
+cat("\nReconstructing paleolongitudes (x) & paleolatitudes (y) for ", length(ages), " ages, ending at ", max(ages), "...", sep="")
+i=1
+for (i in 1:length(ages))
+	{
+	# Nice to print each step so you can see timing
+	cat(ages[i], ",", sep="")
+	
+	tmpresult = rgplates::reconstruct(x=xls[,c("long","lat")], age=ages[i], model="MERDITH2021")
+	
+	# Change the names of the columns to specify age
+	colnames(tmpresult) = paste0(c("long_","lat_"), ages[i])
+	tmpresult
+	
+	# Add tmpresult to table
+	reconstructed_points_table = cbind(reconstructed_points_table, tmpresult)
+	
+	}
+	
+reconstructed_points_table
+
+head(reconstructed_points_table[,1:7])
+tail(reconstructed_points_table[,1:7])
+
+
+# Create a new workbook
+wb <- createWorkbook()
+
+# Add a sheet and write the data
+addWorksheet(wb, "Reconstructed_Points_50mya")
+writeData(wb, "Reconstructed_Points_50mya", reconstructed_points_table)
+
+# Save the workbook
+saveWorkbook(wb, "reconstructed_points.xlsx", overwrite = TRUE)
+
